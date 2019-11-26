c059fb19d5717762ec18e3698cbeebb09c33f4fb
Shane <shanemcneil19@gmail.com>
2019-11-26 13:40:54 +0000

    ensure correct db url

--- test/setup.js
+++ test/setup.js
@@ -6,7 +6,7 @@ const { expect } = require('chai')
 const supertest = require('supertest')
 
 process.env.TEST_DB_URL = process.env.TEST_DB_URL
-  || "postgresql://dunder_mifflin:dunder@localhost/thingful-test"
+  || "postgresql://thingful:thingful@localhost/thingful-test"
 
 global.expect = expect
 global.supertest = supertest
